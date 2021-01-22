e18b0b20e625240a3417a4b25e32157d68df26ed
TalaatMagdy <talaatmagdy75@gmail.com>
2021-01-23 00:25:01 +0200

    add notes and start with basic

---
+++ basic/basic_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basic_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
