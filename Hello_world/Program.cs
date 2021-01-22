59e6a909c3bda0e2cf556cee3abed6a5bcd57bde
TalaatMagdyX <tmagdy@lucidya.com>
2021-01-22 05:10:27 +0200

    Hello world In C#

---
+++ Hello_world/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace book_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+        }
+    }
+}
